93179276d70716752f5f01159d78dc2bfdeefbc3
sergiopatinha <patinha.sergio@gmail.com>
2025-10-05 19:48:54 +0100

    Criaçao de env e instalaçoes

---
+++ test_env.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("✅ Ambiente Python a funcionar com sucesso!")
+print("Pandas:", pd.__version__)
+print("Numpy:", np.__version__)
